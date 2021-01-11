c62060188527903b125ceae5741d3a79f02e4566
mohamoud43 <mohamoud43@gmail.com>
2021-01-11 15:12:17 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div
+      v-for="(home, index) in homes"
+      :key="index"
+      style="float:left;margin:10px"
+    >
+      <router-link :to="`/home/${home.objectID}`"
+        ><home-card :home="home"
+      /></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes";
+
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [
+        {
+          name: "description",
+          content: "This is a homepage!",
+          hid: "description",
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 4),
+    };
+  },
+};
+</script>
